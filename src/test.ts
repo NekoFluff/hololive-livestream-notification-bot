494384049d5c5db07518ae1156c007b7b4efb6f8
Alex Nou <alexnou@gmail.com>
2020-09-25 11:22:51 -0700

    Use new refactored code

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,20 @@
 import transmitDiscordNotification from "./discord/transmitDiscordNotification";
 import fs from "fs";
 import discordBot from "./discord/discordBot";
-import parseYoutubeXML from "./discord/parseYoutubeXML";
+import parseYoutubeXMLIntoFeedData from "./discord/parseYoutubeXMLIntoFeedData";
+import createEmbed from "./discord/createEmbed";
 
 discordBot.on("ready", () => {
   fs.readFile(__dirname + "/foo.xml", async function (err, data) {
-    const embed = await parseYoutubeXML(data);
-    if (embed && embed.author)
-      transmitDiscordNotification(embed.author.name || "", embed);
+    const feedData = await parseYoutubeXMLIntoFeedData(data);
+    if (feedData) {
+      const embed = createEmbed(
+        feedData.title,
+        feedData.author,
+        feedData.authorURL,
+        feedData.link
+      );
+      transmitDiscordNotification(feedData.author || "", embed);
+    }
   });
 });
