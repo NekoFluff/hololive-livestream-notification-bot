a41d05f4a1d083e5ea2e91fb44267d875233b005
Alex Nou <alexnou@gmail.com>
2021-07-17 19:29:16 -0700

    Make subscribe command smarter

--- src/discord/commands/subscribe.ts
+++ src/discord/commands/subscribe.ts
@@ -1,16 +1,40 @@
 import { Command } from "discord-messenger";
 import SubscriptionsRepository from "./../../repos/SubscriptionsRepository";
 
-const subscriptionRepo = new SubscriptionsRepository();
+const subscriptionsRepo = new SubscriptionsRepository();
 
 const SubscribeCommand: Command = {
   name: "subscribe",
   description:
     "Recieve notifications by direct messages (DMs) for a hololive-EN member\n`!subscribe gawr gura amelia watson`",
   async execute(msg, args) {
-    const result = await subscriptionRepo.addSubscriptions(
+    const authors = args.map((value) => value.toLocaleLowerCase());
+    const existingSubs = Object.values(await subscriptionsRepo.getSubscriptions(msg.author.id)).map((sub) => { return sub['subscription'] });
+    const duplicates: string[] = [];
+    const newAuthors = authors.filter(function (author) {
+      if (existingSubs.indexOf(author) < 0) {
+        duplicates.push(author);
+        return false; // Don't include
+      }
+      return true;
+    });
+
+    if (duplicates.length > 0) {
+      msg.reply(
+        `Looks like you're already subscribed to these: ${duplicates.join(", ")}`
+      );
+    }
+
+    if (newAuthors.length === 0) {
+      msg.reply(
+        `I don't see any new subscription names in the list you provided. Did you type everything in right?`
+      );
+      return;
+    }
+
+    const result = await subscriptionsRepo.addSubscriptions(
       msg.author.id,
-      args.map((value) => value.toLocaleLowerCase())
+      newAuthors
     );
     if (result && result.insertedCount > 0) {
       msg.reply("Subscribed to " + args);
